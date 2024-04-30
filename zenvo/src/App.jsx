9ca6ab3c84ef9264846477e9c7cdb78bb0abd77a
sacuadros09 <sacuma09@gmail.com>
2024-04-30 10:16:47 -0500

    fixederrors

--- zenvo/src/App.jsx
+++ zenvo/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { AppRouter } from "./router/AppRouter"
 import { Header } from './components'
-
+  
 
 
 function App() {
--- zenvo/src/router/AppRouter.jsx
+++ zenvo/src/router/AppRouter.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom"
 import { AboutUs,ServiceOur} from '../pages';
 
 
 export function AppRouter() {
   return (
-    <Router>
+
       <Routes>
         <Route path='/' element={<AboutUs />} />
         <Route path='/serviceour' element={<ServiceOur />} />
         <Route path='/*' element={<p>404</p>} />
       </Routes>
-    </Router>
+
   );
 }
