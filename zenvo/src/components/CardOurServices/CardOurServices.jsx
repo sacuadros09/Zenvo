2ac1123c49b4c11d885bf10493fe172a350c87f9
sacuadros09 <sacuma09@gmail.com>
2024-05-29 11:52:54 -0500

    css de services

--- zenvo/src/components/CardOurServices/CardOurServices.jsx
+++ zenvo/src/components/CardOurServices/CardOurServices.jsx
@@ -1,11 +1,21 @@
 import './CardOurServices.css'
+
+import { Link } from 'react-router-dom'
+
 export function CardOurServices (props) {
-    const { title, description, img, url,variant } = props
+    const { title, description, img, url,variant, reverse, path, text } = props
     return (
-      <div className={variant}>
+      <div className={`${variant} ${reverse ? 'reverse' : ''}`}>
         <img src={img} />
+        <div className="text-container">
         <h1> {title} </h1>
         <p>{description}</p>
+        <Link to={path}>
+            <button className="contactus-button-service">
+                <h5 className='text-service'>{text}</h5> 
+            </button>
+        </Link>
+        </div>
       </div>
     )
   }
--- zenvo/src/pages/OurServices/OurServices.jsx
+++ zenvo/src/pages/OurServices/OurServices.jsx
@@ -1,36 +1,39 @@
 import "./OurServices.css"
-import { InfoText, Button, CardOurServices} from "../../components"
+import { InfoText,CardOurServices} from "../../components"
 import { dataServices } from "../../services/dataServices"
 
 export function OurServices () {  
     
-return(
-    <div className="big-services">
-        <div className="navbar-space"/>
-<div className="who-we-are">
-        <InfoText variant="infotext-ourservices" title = "Our services"  text = "Welcome to our services section at Zenvo Studio! This is where the magic happens. Dive into our creative world and discover how we can help you take your project to the next level. From web design to branding, we're here to offer you visual solutions that captivate, inspire and make your brand shine. Explore our services below and let us take your ideas beyond your expectations." img="/src/assets/RobotServices.png"></InfoText>
-        </div>
-        <div className="who-we-celphone">
-        <InfoText variant="infotext-celphone" title = "Our services"  text = "Welcome to our services section at Zenvo Studio! This is where the magic happens. Dive into our creative world and discover how we can help you take your project to the next level."></InfoText>
-        </div>
-        <div className="transform">
-            <h2 className="transform-letter">"We transform ideas into exceptional digital experiences, where creativity and innovation are in every pixel."</h2>
+    return(
+        <div className="big-services">
+            <div className="navbar-space"/>
+    <div className="who-we-are">
+            <InfoText variant="infotext-ourservices" title = "Our services"  text = "Welcome to our services section at Zenvo Studio! This is where the magic happens. Dive into our creative world and discover how we can help you take your project to the next level. From web design to branding, we're here to offer you visual solutions that captivate, inspire and make your brand shine. Explore our services below and let us take your ideas beyond your expectations." img="/src/assets/RobotServices.png"></InfoText>
+            </div>
+            <div className="who-we-celphone">
+            <InfoText variant="infotext-celphone" title = "Our services"  text = "Welcome to our services section at Zenvo Studio! This is where the magic happens. Dive into our creative world and discover how we can help you take your project to the next level."></InfoText>
+            </div>
+            <div className="transform">
+                <h2 className="transform-letter">We transform ideas into exceptional digital experiences, where creativity and innovation are in every pixel.</h2>
+            </div>
+            
+          <div className="services-container">
+        {dataServices.map(({id,img,title,description, path},index) => {
+        return(
+        <div key={id} className="services-card">
+        <CardOurServices variant="cardservices-ourservices" img={img} title={title} description={description}  reverse={index % 2 !== 0} path={path }  text="See more" url="/UxServicePage"></CardOurServices>  
         </div>
         
-      <div className="services-container">
-    {dataServices.map(({id,img,title,description,path}) => {
-    return(
-    <div key={id} className="services-card">
-    <CardOurServices variant="cardservices-ourservices" img={img} title={title} description={description} ></CardOurServices>  
-    <Button path={path }variant="our-button" text="see more" url="/UxServicePage"/>
-    <div>
-   
-    </div>
-    </div>
+        )
+        })}
+    
+        </div>
+        <img className="decoration-service1" src="../../../src/assets/circle-line-service.png"/>
+        <img className="decoration-service2" src="../../../src/assets/circle-line-service2.png"/>
+        <img className="decoration-service3" src="../../../src/assets/circle-line-service.png"/>
+        </div>  
+    
+    
     
     )
-    })}
-    </div>
-    </div>  
-)
-}
+    }
